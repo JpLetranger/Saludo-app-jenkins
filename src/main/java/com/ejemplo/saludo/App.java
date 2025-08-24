bbbc0f31401dc85d7f0c4e42a61972497f988dc3
Juanpablo <juangajardonov@gmail.com>
2025-08-23 21:14:09 -0400

    Agregar método saludoCompleto

--- src/main/java/com/ejemplo/saludo/App.java
+++ src/main/java/com/ejemplo/saludo/App.java
@@ -1,12 +1,9 @@
 package com.ejemplo.saludo;
 
 public class App {
-    public static String generarSaludo(String nombre) {
-        if (nombre == null || nombre.trim().isEmpty()) {
-            return "¡Hola, Mundo!";
-        }
-        return "¡Hola, " + nombre.trim() + "!";
-    }
+    public static String saludoCompleto(String nombre) {
+    return generarSaludo(nombre) + " Bienvenido a Jenkins!";
+}
     
     public static void main(String[] args) {
         String nombre = args.length > 0 ? args[0] : "Jenkins";
